fce477a69e5536f32026a8d5350c59e115cd1f66
BO <napawan-bo@hotmail.com>
2023-09-21 11:55:18 +0700

    add on

--- service/connectpostgres.py
+++ service/connectpostgres.py
@@ -1,16 +1,26 @@
 import psycopg2
 
+#local database
+# def connectdb():
+#     connection = psycopg2.connect(
+#         host='localhost',
+#         user='postgres',
+#         password='1111',
+#         database='sampledb',
+#         port=5432,
+#     )
+#     return connection
 
+#server database
 def connectdb():
     connection = psycopg2.connect(
-        host='localhost',
-        user='postgres',
-        password='1111',
+        host='dpg-ck5snk8s0i2c73bv6p2g-a',
+        user='admin',
+        password='XhiFJIScQB4EB9iSabRmp8gnx113sA15',
         database='sampledb',
         port=5432,
     )
     return connection
-
 
 # call function
 print(connectdb())
